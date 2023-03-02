3e958164a14faca4149bd77761222a5d794edaac
Gribnoi17 <76908537+Gribnoi17@users.noreply.github.com>
2023-03-03 00:12:54 +0300

    Lesson1 готово!

---
+++ Lesson1/buttonclicker/src/main/java/com/mirea/gribanovdv/buttonclicker/MainActivity.java
@@ -0,0 +1,42 @@
+package com.mirea.gribanovdv.buttonclicker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tvOut;
+    private Button btnWhoAmI;
+    private Button btnItIsNotMe;
+    private CheckBox check;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvOut = findViewById(R.id.tvOut);
+        btnWhoAmI = findViewById(R.id.btnWhoAmI);
+        btnItIsNotMe = findViewById(R.id.btnItIsNotMe);
+        check = findViewById(R.id.checkBox);
+
+        View.OnClickListener oclBtnWhoAmI = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvOut.setText("Мой номер по списку № 7");
+                check.setChecked(true);
+            }
+        };
+        btnWhoAmI.setOnClickListener(oclBtnWhoAmI);
+    }
+
+
+    public void onMyButtonCLick(View view) {
+        tvOut.setText("Это не я сделал!");
+        check.setChecked(false);
+    }
+}
